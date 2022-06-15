0d6ae0e035243badcba0c6234ed107f61cee1c80
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2022-06-14 23:32:53 -0300

    criando um esboço do projeto

---
+++ main.py
@@ -0,0 +1,24 @@
+import PyPDF2
+import re
+
+#Passamos o caminho do arquivo
+caminho_arquivo = open('Binding.pdf', 'rb')
+#Aqui vai ser a lib vai fazer a leitura do pdf informado no caminho
+leitura_pdf = PyPDF2.PdfFileReader(caminho_arquivo)
+#Aqui escolhemos o numero da pagina de onde vamos capturar os dados
+numero_da_pagina = leitura_pdf.getNumPages()
+escolhe_pagina = leitura_pdf.getPage(1)
+#Aqui o texto da pagina selecionada é extraido
+conteudo = escolhe_pagina.extractText()
+#Aqui é feita a junção
+pega_texto = ''.join(conteudo)
+#Isso remove a quebra de linhas
+junta_texto = re.sub('n', '', pega_texto)
+print("\nPegando o texto todo com quebra de linha")
+print(junta_texto)
+print("\n-----------")
+#Isso pega as primeira 50 posições da pagina selecionada e imprimi
+print("Pegando apenas as 50 primeiras posições")
+novaString = junta_texto[0:50]
+print(novaString)
+
